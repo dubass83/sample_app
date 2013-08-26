d5ca4aeee94093b4fd58d06ccb2dd236ebc32c1b
dubass83 <makssych@gmail.com>
2013-08-26 22:38:37 +0300

    26.08.2013 work on signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,4 +17,28 @@ describe "UserPages" do
     it { should have_selector('h1', text: user.name) }
     it { should have_selector('title', text: user.name) }
   end
+
+  describe "signup" do 
+    before { visit signup_path }
+    let(:submit) { "Create my account" }
+    
+    describe "With invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "With valid information" do
+      before do
+        fill_in "Name", with: "Example_User"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end 
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
