5d53b6606676d7f767d278593b51d83c628d4a26
dubass83 <makssych@gmail.com>
2013-09-01 21:38:39 +0300

    begining of the 8 chapter

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
 resources :users
+resources :sessions, only: [ :new, :create, :destroy ]
 
 #  get "users/new"
 
@@ -15,7 +16,10 @@ root to: "static_pages#home"
 
 match '/signup', to: "users#new"
 
+match '/signin', to: "sessions#new"
 
+match '/signout', to: "sessions#destroy", via: :delete 
+ 
 match '/help', to: "static_pages#help"
 
 match '/about', to: "static_pages#about"
