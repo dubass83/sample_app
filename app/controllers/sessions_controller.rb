eac6c2c60932f9930c8a83386aa6fda6ba6452c3
dubass83 <makssych@gmail.com>
2013-09-10 20:59:39 +0300

    begining of the chapter 8.2.5

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,10 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:session][:email].downcase)
-    if user && user.autheticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       #Sign in user and redirec to user's show page
+      sign_in user
+      redirect_to user
     else
       flash.now[:error] = "Invalid email/password combination" 
       render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,14 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
+#  remember_token  :string(255)
 #
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
+  before_save :create_remember_token
   before_save { |user| user.email = email.downcase  }
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -21,4 +23,11 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sansetive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  private
+ 
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
+
 end
